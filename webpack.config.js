700225d2c7acb0c264ff3a33d9ac3983b49a6436
Deivid Veloso <veloso.deivid@gmail.com>
2020-11-24 12:06:30 -0300

    migrate v4 to v5 from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,12 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    // compile both unminified and minified dist files from same source
     'telnyx-rtc-sipjs': './src/telnyx-rtc-sipjs.js',
     'telnyx-rtc-sipjs.min': './src/telnyx-rtc-sipjs.js',
-    //     // Make bundles with all deps included
-    //     // "telnyx-rtc-sipjs.bundle": ["./src/telnyx-rtc-sipjs.js", "./node_modules/sip.js/src/SIP.js"],
-    //     // "telnyx-rtc-sipjs.bundle.min": ["./src/telnyx-rtc-sipjs.js", "./node_modules/sip.js/src/SIP.js"],
   },
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
@@ -22,22 +20,14 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader?presets[]=es2015',
-      },
-      // needed for sip.js to load correctly with webpack:
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
       },
     ],
   },
   plugins: [
     new webpack.ProvidePlugin({
       SIP: 'sip.js',
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true,
     }),
   ],
 };
